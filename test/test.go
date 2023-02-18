f50050fc27f202d54e269532aa4695e55da93e8f
ATXTKS <79209748@qq.com>
2023-02-18 17:11:10 +0800

    wsl装了redis，这里的test连接上了，并且设置了键值对

--- test/test.go
+++ test/test.go
@@ -1,10 +1,40 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"github.com/go-redis/redis"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	http.ListenAndServe(":8080", nil)
+	// 创建Redis客户端
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // Redis服务器地址
+		Password: "",               // Redis密码，如果没有则留空
+		DB:       0,                // Redis数据库索引（0表示默认数据库）
+	})
+
+	// 检查Redis连接是否正常
+	_, err := client.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
+
+	// 在Redis中设置一个键值对
+	err = client.Set("T-ara", "Park.Jiyeon", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	// 从Redis中获取一个键值对
+	val, err := client.Get("T-ara").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("T-ara:", val)
+
+	// 关闭Redis客户端
+	err = client.Close()
+	if err != nil {
+		panic(err)
+	}
 }
